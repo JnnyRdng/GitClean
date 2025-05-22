1e29ea6b889a2a743c4333ce6c103198b86e9d52
Jonny Reading <jonny@firefinch.io>
2025-05-22 14:52:56 +0100

    Program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace GitClean
+﻿using GitClean.Commands;
+using Spectre.Console.Cli;
+
+namespace GitClean
 {
     public static class Program
     {
         public static int Main(string[] args)
         {
-            return 0;
+            var app = new CommandApp<CleanCommand>()
+                .WithDescription("Removes local branches in a git repository that no longer exist on the remote.");
+
+            app.Configure(config =>
+            {
+                config.SetApplicationName("GitClean");
+            });
+            try
+            {
+                return app.Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return 1;
+            }
         }
     }
 }
